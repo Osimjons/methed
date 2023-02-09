acb28de6f149048b34d0e984b7fb527c79fc4c77
Osimjons <74949726+Osimjons@users.noreply.github.com>
2023-02-09 14:19:00 +0300

    Add lesson_1

---
+++ Lesson_1/js/index.js
@@ -0,0 +1,12 @@
+/*Обязательный блок  */
+const product = "Imac";
+console.log(product);
+const quantityProduct = 12;
+console.log(quantityProduct);
+const categoryProduct = "laptop";
+console.log(categoryProduct);
+const priceProduct = 12500;
+console.log(priceProduct);
+/* Не обязательный блок  */
+const result = quantityProduct * priceProduct;
+console.log("Общая стоимость товаров " + result + "₽");
